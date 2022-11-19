af004a06749f03e45368c39acf50fbc23e27272d
Paul McQuade <paulmcquad@gmail.com>
2022-11-19 17:43:38 +0000

    Primes Numbers and Factoring
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ Chapters/2_Prime_No_Factorization/fta.py
@@ -0,0 +1,27 @@
+# Unique Factorization of Integers
+# https://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic
+
+# Description: Print all the prime factors of a given number.
+# Author     : Paul McQuade <paulmcquad@gmail.com>
+# License    : GPLv3
+
+def main():
+    print("Type in No. to factor:")	
+    n = int(input())
+    i = 2
+    out = ""
+	
+    while not 1 == n:
+        
+        fm = 0
+        while n % i == 0:
+            fm += 1
+            n /= i
+		
+        if fm:
+            out += str(i) + "^" + str(fm) + "*"
+        i += 1
+
+    print(out[:-1])	  
+
+main()
